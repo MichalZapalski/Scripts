3ba457fd1d531d3bb228f6aec4a357aacee26ac5
MichalZapalski <105287826+MichalZapalski@users.noreply.github.com>
2026-01-22 12:29:59 +0100

    Delete StorageHandler.ps1

--- StorageHandler.ps1
+++
@@ -1,7 +0,0 @@
-﻿Connect-AzAccount 
-
-Set-AzContext -Subscription "ets-wrkp-avd-001"
-$ctx=New-AzStorageContext -StorageAccountName "etsstavdfslgx01" -StorageAccountKey "p7/UyYDZb2ypNonigRwNwnV+2YWDfRBY2pIRrkDq/YhdEmbHMs9MhLJNqvQeMPW1ORyP/lCTVtE2ArnPPvswmA==" 
-
-#replace user ID
-Get-AzStorageFileHandle -ShareName "capfslogixprofilecontainer01" -Path "capfslogixprofile01/tdey" -Recursive -Context $ctx
