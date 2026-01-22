a4f8b1b210ba1a1d5da8cfd663f7620cf6bbe72b
MichalZapalski <105287826+MichalZapalski@users.noreply.github.com>
2026-01-22 12:26:48 +0100

    Update Resize_VirtualMachine.ps1

--- Resize_VirtualMachine.ps1
+++ Resize_VirtualMachine.ps1
@@ -1,9 +1,9 @@
 ﻿Connect-AzAccount
 
-Set-AzContext -SubscriptionId "6765aff3-c21b-4e58-aac1-d719cd853dd4"
+Set-AzContext -SubscriptionId ""
 
-$resourceGroupName = "ets-wrkp-rg-InstPort-avd-eastus-001"
-$hostPoolName = "ets-wrkp-hpool-InstPort-prd-pi-avd-eastus-001"
+$resourceGroupName = ""
+$hostPoolName = ""
 
 $vms = Get-AzVm -ResourceGroupName $resourceGroupName
 
@@ -12,7 +12,8 @@ $vmsInHostPool = $vms | Where-Object { $_.Tags.hostpool_name -eq $hostPoolName }
 foreach ($vm in $vmsInHostPool)
 {
             $vm.HardwareProfile.VmSize = 'Standard_E2s_v3'
-            Update-AzVM -VM $vm -ResourceGroupName 'ets-wrkp-rg-InstPort-avd-eastus-001'
+            Update-AzVM -VM $vm -ResourceGroupName ''
             
+
 
 }
